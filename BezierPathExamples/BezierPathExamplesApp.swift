e40c55ced9efd8380cf26e7642c01a4c3554ede8
javff <javff2@gmail.com>
2022-01-10 08:46:56 -0300

    Initial Commit

---
+++ BezierPathExamples/BezierPathExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BezierPathExamplesApp.swift
+//  BezierPathExamples
+//
+//  Created by Juan vasquez on 10-01-22.
+//
+
+import SwiftUI
+
+@main
+struct BezierPathExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
